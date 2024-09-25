53514a8412163e7b1bc2ea922ec2285dd05e5301
michaelj916 <mjarrah916@gmail.com>
2024-09-25 16:16:01 -0700

    Updated for Linux

--- Lab1/README.md
+++ Lab1/README.md
@@ -1,2 +1,14 @@
-# 326
-first commit
+# Group Project 1: Warm up of Interprocess Communication
+In this project, you will design and implement a file-copying program named filecopy.c that utilizes
+ordinary pipes for inter-process communication. The program will be passed two parameters: the name of
+the file to be copied and the name of the destination file. It will create a pipe (please find more info of pipe
+for interprocess communication by yourself), write the contents of the source file to the pipe, and then have
+a child process read from the pipe and write to the destination file.
+
+## How-to run (Using Linux & gcc)
+Ensure `gcc` is installed, if it isn't run: `apt install gcc`
+
+Then run: 
+`gcc filecopy.c -o run`
+(Optionally generate a text file) `echo some text > input.txt`
+`./run input.txt output.txt`
--- Lab1/filecopy.c
+++ Lab1/filecopy.c
@@ -1,102 +1,82 @@
-/**
- * filecopy.c
- * 
- * This program copies files using a pipe.
- *
- */
-
-#include <unistd.h>
 #include <stdio.h>
-#include <fcntl.h>
 #include <sys/types.h>
-#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
-#define READ_END 0
-#define WRITE_END 1
+#define BUFFER_SIZE 50
 
-#define BUFFER_SIZE 10
-
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-	if (argc != 3){
-		printf("Error: Expected 2 arguments \n");
-		exit(1);
-	}
+    int pipe_fd[2];
+    pid_t child_pid;
+    char buffer[BUFFER_SIZE];
 
-	char *sourceFile = argv[1];
-	char *destFile = argv[2];
+    // Check if the correct number of arguments is provided
+    if (argc != 3) 
+    {
+        fprintf(stderr, "ERROR: Need exactly 2 parameters.\n");
+        exit(EXIT_FAILURE);
+    }
 
-	int fd[2];
+    // Open source and target files
+    int source_fd = open(argv[1], O_RDONLY);
+    int target_fd = open(argv[2], O_RDWR | O_CREAT | O_APPEND, 0666);
 
-	if (pipe(fd) == -1 ) {
-		printf("Failed to create Pipe \n");
-		exit(1);
-	}
-	
-	pid_t pid = fork();
+    if (source_fd == -1 || target_fd == -1) 
+    {
+        perror("ERROR: Unable to open file");
+        exit(EXIT_FAILURE);
+    }
 
-	if (pid < 0){
-		printf("Failed to fork process \n");
-		exit(1);
-	}
+    // Create pipe
+    if (pipe(pipe_fd) == -1) 
+    {
+        perror("ERROR: Pipe creation failed");
+        exit(EXIT_FAILURE);
+    }
 
-	if (pid == 0){
-		close(fd[1]);
+    // Fork to create a child process
+    child_pid = fork();
 
-		if (dup2(fd[0], STDIN_FILENO) == -1) {
-			printf("Failed to duplicate file descriptor \n");
-			exit(1);
-        }
+    if (child_pid == -1) 
+    {
+        perror("ERROR: Fork failed");
+        exit(EXIT_FAILURE);
+    }
 
-		close(fd[0]);
+    if (child_pid == 0) // Child process
+    {
+        close(pipe_fd[1]); // Close write-end of the pipe
 
-		execl("./child", "./child", destFile, (char *)NULL);
-
-		printf("Failed to exec child process\n");
-        exit(1);
-} else {
-        // Parent process
-
-        // Close the read end of the pipe
-        close(fd[0]);
-
-        // Open the source file
-        int src_fd = open(sourceFile, O_RDONLY);
-        if (src_fd < 0) {
-            perror("Failed to open source file");
-            // Close the write end before exiting
-            close(fd[1]);
-            exit(1);
-        }
-
-        // Read from source file and write to the pipe
-        char buffer[BUFFER_SIZE];
+        // Read from the pipe and write to the target file
         ssize_t bytes_read;
-
-        while ((bytes_read = read(src_fd, buffer, BUFFER_SIZE)) > 0) {
-            ssize_t bytes_written = write(fd[1], buffer, bytes_read);
-            if (bytes_written != bytes_read) {
-                perror("Failed to write to pipe");
-                close(src_fd);
-                close(fd[1]);
-                exit(1);
-            }
+        while ((bytes_read = read(pipe_fd[0], buffer, sizeof(buffer))) > 0) 
+        {
+            write(target_fd, buffer, bytes_read);
         }
 
-        if (bytes_read < 0) {
-            perror("Failed to read from source file");
-            close(src_fd);
-            close(fd[1]);
-            exit(1);
+        close(pipe_fd[0]); // Close read-end of the pipe
+        close(target_fd);  // Close target file
+    } 
+    else // Parent process
+    {
+        close(pipe_fd[0]); // Close read-end of the pipe
+
+        // Read from the source file and write to the pipe
+        ssize_t bytes_read;
+        while ((bytes_read = read(source_fd, buffer, sizeof(buffer))) > 0) 
+        {
+            write(pipe_fd[1], buffer, bytes_read);
+            memset(buffer, 0, sizeof(buffer));
         }
 
-        // Close the source file and the write end of the pipe
-        close(src_fd);
-        close(fd[1]);
-
-        // Wait for the child process to finish
-        wait(NULL);
+        close(pipe_fd[1]); // Close write-end of the pipe
+        close(source_fd);  // Close source file
+        wait(NULL);        // Wait for child process to finish
     }
 
     return 0;
